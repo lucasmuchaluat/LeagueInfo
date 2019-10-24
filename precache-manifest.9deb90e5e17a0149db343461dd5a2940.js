2355f5dc1a441f5a702e868417cf1a29cd87b88d
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2019-10-24 00:04:31 -0300

    Updates

---
+++ precache-manifest.9deb90e5e17a0149db343461dd5a2940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9157adb08f75abc4b54d0c717c14eb",
+    "url": "/LeagueInfo/index.html"
+  },
+  {
+    "revision": "18235181080dbdfc0cd5",
+    "url": "/LeagueInfo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "93d87309ad160b113673",
+    "url": "/LeagueInfo/static/css/main.07c1af33.chunk.css"
+  },
+  {
+    "revision": "18235181080dbdfc0cd5",
+    "url": "/LeagueInfo/static/js/2.71663b51.chunk.js"
+  },
+  {
+    "revision": "93d87309ad160b113673",
+    "url": "/LeagueInfo/static/js/main.7b64a4ea.chunk.js"
+  },
+  {
+    "revision": "aeea02090fdf134b5f56",
+    "url": "/LeagueInfo/static/js/runtime-main.9be8d17e.js"
+  }
+]);
